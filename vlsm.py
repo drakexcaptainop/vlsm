a2bac1d729f43b7a65a93835d05dc483a356b200
cazadorDeColombianos <90215784+drakexcaptainop@users.noreply.github.com>
2024-11-26 22:19:27 -0400

    Add files via upload

--- vlsm.py
+++ vlsm.py
@@ -11,19 +11,21 @@ parser.add_argument('--use_experimental', action='store_true', default=False)
 parser.add_argument('--hosts_from_file', type=str, default=False)
 parser.add_argument('--host_sep', type=str, default=",")
 parser.add_argument('--concat_multiline', action='store_true', default=False)
+parser.add_argument("--destination", type=str, default="" )
 
 
 args = parser.parse_args()
 
 
-def experimental(f):
-    def wrapper(*qargs, **kwargs):
-        if args.use_experimental:
-            return f(*qargs, **kwargs)
-        else:
-            return addr.step(*qargs, **kwargs)
-    return wrapper
+def experimental(obj_name, alt_method):
+    def decor(f):
+        def wrapper(*qargs, **kwargs):
+            fun = f if args.use_experimental else globals()[obj_name].__dict__[alt_method]
+            return fun(*qargs, **kwargs)
+        return wrapper
+    return decor
 
+class addr: pass
 class addr:
     def __init__(self, base, mask, host_requested=None) -> None:
         self.base: list[int] = base 
@@ -39,9 +41,9 @@ class addr:
 
     def step_next_usable_net(self, for_hosts=None):
         _addr = self.copy()
-        _addr.step( 256 - self.base[-1] )
+        _addr.step_optimized( 256 - self.base[-1] )
         if for_hosts is not None:
-            _addr.mask = int2list_mask( host_requested_to_mask(for_hosts) )
+            _addr._set_mask( host_requested_to_mask(for_hosts) )
         return _addr
 
     def _set_mask(self, mask):
@@ -69,7 +71,7 @@ class addr:
                 v = 0
             v -= iav 
     
-    @experimental
+    @experimental( obj_name='addr', alt_method='step' )
     def step_optimized(self, v):
         base_change_index = 1
         q = v + self.base[-1]
@@ -80,7 +82,7 @@ class addr:
             self.base[-base_change_index] = 0
             base_change_index += 1
             q += self.base[-base_change_index]
-        self.base[-base_change_index] += zero_center_div
+        self.base[-base_change_index] += max(1, zero_center_div)
                 
     def __repr__(self) -> str:
         return str(self)
@@ -161,14 +163,17 @@ mask = 24
 _addr = addr( str2list_addr(args.net), int2list_mask(mask) )
 hosts = [int(v) for v in args.hosts.split(",")] if not args.use_parser else parse_hosts()
 
+file = open(args.destination, "w") if args.destination else None
+
 if isinstance(hosts[0], list):
     _addr._set_mask( host_requested_to_mask( sum(hosts[0]) ) )
     for i, hostsarr in enumerate(hosts):
-        print(F"VLSM {i+1} - #BaseAddress [{".".join( [str(v) for v in _addr.base] )} / {list2id_mask(_addr.mask)}]:")
+        print(F"VLSM {i+1} - #BaseAddress [{".".join( [str(v) for v in _addr.base] )} / {list2id_mask(_addr.mask)}]:", file=file)
         _vlsm = vlsm(_addr, hostsarr)
-        print("\n".join(f"{i+1}) {str(a)}" for i, a in enumerate(_vlsm)),end="\n\n")
+        print("\n".join(f"{i+1}) {str(a)}" for i, a in enumerate(_vlsm)),end="\n\n", file= file)
         _addr = _vlsm[-1].step_next_usable_net( sum(hostsarr) )
-        
-        
+    if file:
+        file.flush()    
+        file.close()
 else:
     print("\n".join(f"{i+1}) {str(a)}" for i, a in enumerate(vlsm(_addr, hosts))))
